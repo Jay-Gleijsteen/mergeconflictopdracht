356cbd741c533a27c825e0da4a6777b2cd149afe
jay gleijsteen <36629@ma-web.nl>
2023-10-16 12:54:40 +0200

    nieuwe commi

--- js/main.js
+++ js/main.js
@@ -9,9 +9,14 @@ function basicHaiku()
     return ["Toward those short trees","We saw a Spiderman descending","On a day in summer."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
+function basicHaikustudent1()
+{
+    return ["My life, - How much more of it remains? The night is brief."]
+}
 
 haikus = [
     basicHaiku()
+    ,basicHaikustudent1()
 ]
 
 function randomHaiku()
